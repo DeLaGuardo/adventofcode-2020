c30ac71fc671e1df24422dc76cf70dec4e8fae4b
Kirill Chernyshov <delaguardo@gmail.com>
2020-12-03 10:41:56 +0100

    Adjust performance

--- src/aoc/year_2020/day_3.clj
+++ src/aoc/year_2020/day_3.clj
@@ -5,18 +5,17 @@
   (with-open [rdr (io/reader (io/resource "year_2020/input/day_3"))]
     (loop [acc {} i 0 [line & lines] (line-seq rdr)]
       (if line
-        (recur (assoc acc i (cycle (map #(case % \. 0 \# 1) line))) (inc i) lines)
+        (recur (assoc acc i (vec (map #(case % \. 0 \# 1) line))) (inc i) lines)
         acc))))
 
 (defn solution [jj ii]
   (let [m (read-input)
-        max-k (apply max (keys m))]
-    (loop [acc 0 i 0 j 0]
-      (if (= j max-k)
-        acc
-        (recur (+ acc (nth (get m (+ j jj)) (+ i ii)))
-               (+ i ii)
-               (+ j jj))))))
+        max-j (apply max (keys m))
+        max-i (count (get m 0))]
+    (reduce +' (map (fn [i j]
+                      (get-in m [j (mod i max-i)]))
+                    (iterate #(+ % ii) 0)
+                    (range 0 (inc max-j) jj)))))
 
 (defn solution-1 []
   (solution 1 3))
@@ -26,8 +25,14 @@
 
 (comment
 
-  (solution-1)
+  (read-input)
 
-  (solution-2)
+  (time
+   (dotimes [_ 100]
+     (solution-1)))
+
+  (time
+   (dotimes [_ 100]
+     (solution-2)))
 
   )
